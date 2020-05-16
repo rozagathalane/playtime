a83ee165354ddc5b7e6d661898a6a4d02a6248e7
Kevin Torrington <kevin.torrington@administrators-MacBook-Pro.local>
2020-05-16 13:02:16 +0100

    Bugs to fix.

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const myName = 'roz';
 let myAge = 10;
 
-let intro = 'my name is '+ myName + ' and i am '+ myAge + ' years old';
+let intro = 'my name is ' + myName + ' and i am ' + myAge + ' years old';
+
+// Bug 1
+console.log(hello);
+
+// Bug 2.
+const myNumber = 10;
+myNumber = 11;
+
+
+// Bug 3
+console.log(myNumber + myFavfood);
+
+
+
+
+
 
 
 
