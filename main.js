70b0b4ab6d3ff9a5e0d00895a33e3cca19762cc5
roz <roz@timeforabrew.com>
2020-05-16 14:31:48 +0100

    added text images and background

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const myName = 'roz';
 let myAge = 10;
+let myPet = 'hamsters';
+let numberOfHamsters = 3;
+let hamsterName1 = 'Stuffin';
+let hamsterName2 = 'Cookie';
+let hamsterName3 = 'Amber';
+let myColour1 = 'red';
+let myColour2 = 'blue';
+let intro = `My name is ${myName} and i am ${myAge} years old.`;
+let aboutMe = `My favorite pets are ${myPet} and i have ${numberOfHamsters} of them. there names are ${hamsterName1}, ${hamsterName2} and ${hamsterName3}, and my favorite colours are ${myColour1} and ${myColour2}.`;
 
-let intro = 'my name is ' + myName + ' and i am ' + myAge + ' years old';
+
 
 
 
@@ -18,5 +27,6 @@ let intro = 'my name is ' + myName + ' and i am ' + myAge + ' years old';
 
 function hello() {
     document.getElementById("intro").innerHTML = intro;
+    document.getElementById('aboutme').innerHTML = aboutMe; 
 }
 hello();
