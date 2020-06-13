2b394a2a60fed651ad6c46c986949a61c85c8b7a
roz <roz@timeforabrew.com>
2020-06-13 14:25:27 +0100

    remove stuffin

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const myName = 'roz';
 let myAge = 10;
 let myPet = 'hamsters';
-let numberOfHamsters = 3;
-let hamsterName1 = 'Stuffin';
-let hamsterName2 = 'Cookie';
-let hamsterName3 = 'Amber';
+let numberOfHamsters = 2;
+let hamsterName1 = 'Cookie';
+let hamsterName2 = 'Amber';
 let myColour1 = 'red';
 let myColour2 = 'blue';
 let myFriendsAndFamily = `i love my family and friends.`;
 let intro = `My name is ${myName} and i am ${myAge} years old.`;
-let aboutMe = `My favorite pets are ${myPet} and i have ${numberOfHamsters} of them. there names are ${hamsterName1}, ${hamsterName2} and ${hamsterName3}, and my favorite colours are ${myColour1} and ${myColour2}.${myFriendsAndFamily}` ;
+let aboutMe = `My favorite pets are ${myPet} and i have ${numberOfHamsters} of them. there names are ${hamsterName1} and ${hamsterName2}  , and my favorite colours are ${myColour1} and ${myColour2}.${myFriendsAndFamily}` ;
 let sum = 0;
 
 
