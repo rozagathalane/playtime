bf190208b9e7a33651740b0fdcb46dc9e1c4c1e5
roz <roz@timeforabrew.com>
2020-07-25 12:45:29 +0100

    homework

---
+++ homework.js
@@ -0,0 +1,14 @@
+// Use google to create a function that checks the day using a swich statement and prints to the console.
+
+document.write('test');
+switch (monday){
+Monday:
+console.log('its monday');
+break;
+Tuesday:
+console.log('it is tuesday.')
+}
+
+
+
+
---
+++ test.js
@@ -0,0 +1,32 @@
+let age = 11;
+let number = 101;
+const name = 'roz';
+age++;
+console.log(age);
+console.log(`my name is ${name} and i am ${age} years old.`);
+console.log(number + age);
+
+if (age < 10) {
+    console.log('is less than ten.');
+} else {
+    console.log('is less than ten.');
+};
+
+switch(b) {
+case a:
+    console.log('it is a.');
+break;
+case b:
+    console.log('it is b.')
+break;
+case c:
+    console.log('it is c.');
+break;
+default:
+console.log('it is not a letter.')
+break;
+};
+function apple(n,c){
+console.log(`i have ${n} ${c} apples. `)
+};
+apple(5,'red');
