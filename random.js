b3957544d82e4a1f54ec27d48b179f22170dfe04
roz <roz@timeforabrew.com>
2020-11-07 13:49:31 +0000

    changes2

---
+++ random.js
@@ -0,0 +1,13 @@
+//use a method to create a random number and then perform an if statement to say if the number is above or bellow 1 
+
+//console.log(Math.random()*100);
+
+let randomNumber = Math.floor(Math.random()*100);
+
+if (randomNumber>50) {
+    console.log('the catapillar pooed on the bed')
+} else {
+    console.log('the catapillar pooed on the floor')
+};
+
+
