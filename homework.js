b80ddd24ed7a59574c54eadab2bbe6a63a0ea3d0
roz <roz@timeforabrew.com>
2020-11-07 13:11:47 +0000

    CHANGES

--- homework.js
+++ homework.js
@@ -1,14 +1,44 @@
 // Use google to create a function that checks the day using a swich statement and prints to the console.
 
+
+
+
+var d = new Date().getDay();
+
+
+
 document.write('test');
-switch (monday){
-Monday:
-console.log('its monday');
-break;
-Tuesday:
-console.log('it is tuesday.')
-}
 
 
+
+
+switch(new Date().getDay()) {
+case 1:
+    console.log('its monday.');
+    break;
+case 2:
+    console.log('it is tuesday.');
+    break;
+case 3:
+    console.log('it is wednesday.');
+    break;
+case 4:
+    console.log('it is Thursday.');
+    break;
+case 5:
+    console.log('it is friday.');
+    break;
+case 6:
+    console.log('it is saturday.');
+    break;
+case 0:
+    console.log('it is sunday.');
+    break;
+default:
+    console.log('it is not any day.');
+    break;
+};
+
+console.log(d);
 
 
---
+++ reminder.js
@@ -0,0 +1,5 @@
+const myName = 'roz';
+let myUnclesChosenNumber = 39;
+myUnclesChosenNumber++;
+console.log(myUnclesChosenNumber);
+
